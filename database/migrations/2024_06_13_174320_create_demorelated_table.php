4aeef513b91e891be8b38f2fafe5740c39fa051a
anilwagle808 <anil.wagle808@gmail.com>
2024-06-13 23:41:39 +0545

    learned to use foreign key on database

---
+++ database/migrations/2024_06_13_174320_create_demorelated_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('demorelated', function (Blueprint $table) {
+            $table->id();
+            $table->text('comment');
+            $table->unsignedBigInteger('customer');
+            $table->foreign('customer')->references('customer_id')->on('customers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('demorelated');
+    }
+};
